ce9b36c63992ac4eae28d481e58f3d417939d418
lvjx <finalljx@gmail.com>
2013-08-24 17:40:25 +0800

    增加invokeClientRequrest 支持on error

--- config/web/config.js
+++ config/web/config.js
@@ -5,9 +5,9 @@
 			"apple":{"url":"/view/install/ios/manifest.plist","version":"1.007"},
 			"android":{"url":"/view/install/android/HoriMobile.apk","version":"2012122302"},
 			"oaServerName":"V7dev/DigiWin",
-			"serverBaseUrl":"http://localhost:8080/tm/",
+			"serverBaseUrl":"http://localhost:80/tm/",
 			"encryptKey":"horiTech",
-			"appServerHost":"http://localhost:8080/tm/"
+			"appServerHost":"http://localhost:80/tm/"
 		}
 	}
 })($.hori)
