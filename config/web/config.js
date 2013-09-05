447e67f5538d15cc06b36c3a7e8edef7968c9de1
lvjx <finalljx@gmail.com>
2013-09-05 22:31:25 +0800

    提交增加oa多服务器

--- config/web/config.js
+++ config/web/config.js
@@ -2,12 +2,12 @@
 	hori.getconfig=function(){
 
 		return{
-			"apple":{"url":"/view/install/ios/manifest.plist","version":"1.007"},
-			"android":{"url":"/view/install/android/HoriMobile.apk","version":"2012122302"},
 			"oaServerName":"V7dev/DigiWin",
+			"oaMsgServer":"V7dev/DigiWin",
+			"oaMobileServer":"V7dev/DigiWin",
 			"serverBaseUrl":"http://localhost:80/tm/",
 			"encryptKey":"horiTech",
 			"appServerHost":"http://localhost:80/tm/"
-		}
-	}
-})($.hori)
+		};
+	};
+})($.hori);
